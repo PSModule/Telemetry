880b58bb3cdecbc345ed61ae910c62a57233ace0
Marius Storhaug <marstor@hotmail.com>
2026-07-07 17:43:36 +0200

    Require Pester 6.x in test files

--- tests/PSModuleTest.Tests.ps1
+++ tests/PSModuleTest.Tests.ps1
@@ -1,4 +1,6 @@
-﻿Describe 'Module' {
+﻿#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '6.0.0'; MaximumVersion = '6.*'; GUID = 'a699dea5-2c73-4616-a270-1f7abb777e71' }
+
+Describe 'Module' {
     It 'Function: Get-PSModuleTest' {
         Get-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
     }
